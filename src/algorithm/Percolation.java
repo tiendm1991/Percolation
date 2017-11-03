dcb16ea3e518a7c3ec12b1402fc9ca718e51a877
TienDM <TienDM@TienDM>
2017-11-03 22:11:24 +0700

    xxx

--- src/algorithm/Percolation.java
+++ src/algorithm/Percolation.java
@@ -23,55 +23,37 @@ public class Percolation {
 	}
 	
 	public boolean isFull(int row, int col){
-		if (site[row-1][col-1] == 2) return true;
+		if(row == 1 && isOpen(row, col)) return true;
 		int point = xyTo1D(row, col);
 		for(int i = 1; i <= n; i++){
 			int pointCheck = xyTo1D(1, i);
 			if(isOpen(1, i) && quickUnion.connected(point, pointCheck)) {
-				site[row-1][col-1] = 2;
 				return true;
 			}
 		}
 		return false;
 	}
 	
-	
 	public void open(int row, int col){
 		site[row-1][col-1] = 1;
-		if(row == 1) {
-			site[row-1][col-1] = 2;
-		}
-		checkNeighbor(row,col);
-	}
-	
-	private void checkNeighbor(int row, int col){
 		if(isValid(row, col-1)) connect(row, col, row, col-1);
 		if(isValid(row, col+1)) connect(row, col, row, col+1);
 		if(isValid(row-1, col)) connect(row, col, row-1, col);
 		if(isValid(row+1, col)) connect(row, col, row+1, col);
-		
 	}
 	
 	private void connect(int r1, int c1, int r2, int c2){
 		if(isBlock(r2, c2)) return;
 		int point1 = xyTo1D(r1, c1);
 		int point2 = xyTo1D(r2, c2);
-		if(quickUnion.connected(point1, point2)){
+		if(!quickUnion.connected(point1, point2)){
 			quickUnion.union(point1, point2);
-		}
-		if(isFull(r2, c2) && isOpen(r1, c1)) {
-			site[r1-1][c1-1] = 2;
-			checkNeighbor(r1, c1);
-		}
-		if(isFull(r1, c1) && isOpen(r2, c2)) {
-			site[r2-1][c2-1] = 2;
-			checkNeighbor(r2, c2);
 		}
 	}
 
 	public boolean percolates(){
 		for(int i = 0; i < n; i++){
-			if(site[n-1][i] == 2) return true;
+			if(isFull(n, i)) return true;
 		}
 		return false;
 	}
