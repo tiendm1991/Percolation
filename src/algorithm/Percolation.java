d4c8ee8d424767fb4cf083d149772b8b481a50ba
TienDM <TienDm1991@gmail.com>
2017-11-04 11:30:15 +0700

    xxx

--- src/algorithm/Percolation.java
+++ src/algorithm/Percolation.java
@@ -14,10 +14,6 @@ public class Percolation {
 		quickUnion = new WeightedQuickUnionUF(n*n);
 	}
 	
-	public boolean isBlock(int row, int col){
-		return site[row-1][col-1] == 0;
-	}
-	
 	public boolean isOpen(int row, int col){
 		return site[row-1][col-1] == 1;
 	}
@@ -42,15 +38,6 @@ public class Percolation {
 		if(isValid(row+1, col)) connect(row, col, row+1, col);
 	}
 	
-	private void connect(int r1, int c1, int r2, int c2){
-		if(isBlock(r2, c2)) return;
-		int point1 = xyTo1D(r1, c1);
-		int point2 = xyTo1D(r2, c2);
-		if(!quickUnion.connected(point1, point2)){
-			quickUnion.union(point1, point2);
-		}
-	}
-
 	public boolean percolates(){
 		for(int i = 0; i < n; i++){
 			if(isFull(n, i)) return true;
@@ -62,12 +49,25 @@ public class Percolation {
 		int s = 0;
 		for(int i = 0; i < n; i++){
 			for(int j = 0; j < n; j++){
-				if(isFull(i, j)) s++;
+				if(isOpen(i, j)) s++;
 			}
 		}
 		return s;
 	}
 	
+	private boolean isBlock(int row, int col){
+		return site[row-1][col-1] == 0;
+	}
+	
+	private void connect(int r1, int c1, int r2, int c2){
+		if(isBlock(r2, c2)) return;
+		int point1 = xyTo1D(r1, c1);
+		int point2 = xyTo1D(r2, c2);
+		if(!quickUnion.connected(point1, point2)){
+			quickUnion.union(point1, point2);
+		}
+	}
+
 	private boolean isValid(int row, int col){
 		return row >= 1 && row <= n && col >= 1 && col <= n;
 	}
--- src/algorithm/PercolationStats.java
+++ src/algorithm/PercolationStats.java
@@ -1,5 +1,6 @@
 package algorithm;
 
+import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
@@ -15,12 +16,22 @@ public class PercolationStats {
 		
 		for(int i = 0; i < trials; i++){
 			Percolation per = new Percolation(n);
-			results[i] = initPercolation(per);
+			results[i] = initPercolation(i, per);
 		}
 	}
 
-	private double initPercolation(Percolation per) {
-		return 0;
+	private double initPercolation(int i, Percolation per) {
+		int nbOpen = 0;
+		while (!per.percolates()) {
+			int r = StdRandom.uniform(1,n+1);
+			int c = StdRandom.uniform(1,n+1);
+			if(!per.isOpen(r, c)){
+				System.out.println("exp" + (i+1) + ": " + "open (" + r + "," + c + ")");
+				per.open(r, c);
+				nbOpen++;
+			}
+		}
+		return (double) nbOpen/(n*n);
 	}
 
 	public double mean() { 
@@ -32,13 +43,17 @@ public class PercolationStats {
 	}
 
 	public double confidenceLo() {         
-		return mean() - ((1.96 * stddev() / Math.sqrt(trials)));
+		return mean() - 1.96 * stddev() / Math.sqrt(trials);
 	}
 
 	public double confidenceHi() {    
-		return mean() + ((1.96 * stddev() / Math.sqrt(trials)));
+		return mean() + 1.96 * stddev() / Math.sqrt(trials);
 	}
 
 	public static void main(String[] args) {      
+		PercolationStats perStats = new PercolationStats(200, 100);
+		System.out.println(String.format("%-23s = %.16f", "mean", perStats.mean()));
+		System.out.println(String.format("%-23s = %.16f", "stddev", perStats.stddev()));
+		System.out.println(String.format("%-23s = [%.16f, %.16f]", "95% confidence interval",perStats.confidenceLo(), perStats.confidenceHi()));
 	}
 }
