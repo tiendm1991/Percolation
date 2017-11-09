a15ed20592e963be647be458138cb7edaeee2a13
TienDM <TienDM@TienDM>
2017-11-10 02:45:18 +0700

    xxx

--- src/algorithm/Percolation.java
+++ src/algorithm/Percolation.java
@@ -5,7 +5,8 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 	private final int n;
-	private int[][] site;
+	private boolean[][] site;
+	private boolean[][] fullMark;
 	private final WeightedQuickUnionUF quickUnion;
 	private int nbOpen;
 	private boolean percolates;
@@ -15,26 +16,27 @@ public class Percolation {
 			throw new IllegalArgumentException(n + "is invalid!");
 		}
 		this.n = n;
-		this.site = new int[n][n];
+		this.site = new boolean[n][n];
+		this.fullMark = new boolean[n][n];
 		this.quickUnion = new WeightedQuickUnionUF(n * n);
 		this.nbOpen = 0;
 	}
 
 	public boolean isOpen(int row, int col) {
 		checkValid(row, col);
-		return !isBlock(row, col);
+		return site[row - 1][col - 1];
 	}
 
 	public boolean isFull(int row, int col) {
 		checkValid(row, col);
-		if ((row == 1 && !isBlock(row, col)) || site[row - 1][col - 1] == 2) {
-			site[row - 1][col - 1] = 2;
+		if ((row == 1 && !isBlock(row, col)) || fullMark[row - 1][col - 1]) {
+			full(row, col);
 			return true;
 		}
 		int point = xyTo1D(row, col);
 		for (int i = 1; i <= n; i++) {
-			if (!isBlock(1, i) && quickUnion.connected(point, i - 1)) {
-				site[row - 1][col - 1] = 2;
+			if (isOpen(1, i) && quickUnion.connected(point, i - 1)) {
+				full(row, col);
 				return true;
 			}
 		}
@@ -43,10 +45,11 @@ public class Percolation {
 
 	public void open(int row, int col) {
 		checkValid(row, col);
-		if (isBlock(row, col)) {
-			site[row - 1][col - 1] = 1;
-			if (row == 1)
-				site[row - 1][col - 1] = 2;
+		if (!isOpen(row, col)) {
+			site[row - 1][col - 1] = true;
+			if (row == 1) {
+				full(row, col);
+			}
 			nbOpen++;
 			if (isValid(row, col - 1))
 				connect(row, col, row, col - 1);
@@ -59,14 +62,6 @@ public class Percolation {
 			int point = xyTo1D(row, col);
 			if (isFull(row, col)) {
 				for (int j = 1; j <= n; j++) {
-					if (isValid(row, col - 1) && site[row - 1][col - 2] == 1)
-						site[row - 1][col - 2] = 2;
-					if (isValid(row, col + 1) && site[row - 1][col] == 1)
-						site[row - 1][col] = 2;
-					if (isValid(row - 1, col) && site[row - 2][col - 1] == 1)
-						site[row - 2][col - 1] = 2;
-					if (isValid(row + 1, col) && site[row][col - 1] == 1)
-						site[row][col - 1] = 2;
 					if (!percolates && !isBlock(n, j) && quickUnion.connected(point, n * (n - 1) + j - 1)) {
 						percolates = true;
 					}
@@ -83,6 +78,30 @@ public class Percolation {
 		return nbOpen;
 	}
 
+	private void full(int row, int col) {
+		if (!fullMark[row - 1][col - 1]) {
+			fullMark[row - 1][col - 1] = true;
+			fullNeghbor(row, col);
+		}
+
+	}
+
+	private void fullNeghbor(int row, int col) {
+		if (isValid(row, col - 1) && isOpen(row, col - 1)) {
+			fullMark[row - 1][col - 2] = true;
+		}
+		if (isValid(row, col + 1) && isOpen(row, col + 1)) {
+			fullMark[row - 1][col] = true;
+		}
+		if (isValid(row - 1, col) && isOpen(row - 1, col)) {
+			fullMark[row - 2][col - 1] = true;
+		}
+		if (isValid(row + 1, col) && isOpen(row + 1, col)) {
+			fullMark[row][col - 1] = true;
+		}
+
+	}
+
 	private void checkValid(int row, int col) {
 		if (row < 1 || row > n)
 			throw new IllegalArgumentException(row + "outside its prescribed range");
@@ -91,7 +110,7 @@ public class Percolation {
 	}
 
 	private boolean isBlock(int row, int col) {
-		return site[row - 1][col - 1] == 0;
+		return !site[row - 1][col - 1];
 	}
 
 	private void connect(int r1, int c1, int r2, int c2) {
